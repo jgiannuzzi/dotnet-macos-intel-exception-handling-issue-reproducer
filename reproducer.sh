6701a69d7d00b8c8bf60c2ccff5877d6e387de30
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2025-12-09 12:31:26 +0000

    Add support for Linux

--- reproducer.sh
+++ reproducer.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-case $(arch) in
-arm64)
-  native_archs=(arm64 x86_64)
+case $(uname -sm) in
+"Linux aarch64")
+  os=linux
+  osx_archs=()
+  dotnet_archs=(arm64)
+  ;;
+"Linux x86_64")
+  os=linux
+  osx_archs=()
+  dotnet_archs=(x64)
+  ;;
+"Darwin arm64")
+  os=osx
+  osx_archs=(arm64 x86_64)
   dotnet_archs=(arm64 x64)
   ;;
-x86_64)
-  native_archs=(x86_64)
+"Darwin x86_64")
+  os=osx
+  osx_archs=(x86_64)
   dotnet_archs=(x64)
   ;;
 esac
@@ -15,20 +27,20 @@ if [ ! -d .dotnet ]; then
   for arch in "${dotnet_archs[@]}"; do
     echo "===== Installing .NET SDK 9.0 (${arch}) ====="
     mkdir -p ".dotnet/${arch}-9.0-sdk"
-    curl -sSL "https://aka.ms/dotnet/9.0/dotnet-sdk-osx-${arch}.tar.gz" | tar -C ".dotnet/${arch}-9.0-sdk" -xzf -
+    curl -sSL "https://aka.ms/dotnet/9.0/dotnet-sdk-${os}-${arch}.tar.gz" | tar -C ".dotnet/${arch}-9.0-sdk" -xzf -
     echo "===== Installing .NET SDK 9.0 with .NET Runtime 10.0 (${arch}) ====="
     cp -a ".dotnet/${arch}-9.0-sdk" ".dotnet/${arch}-9.0-sdk+10.0-runtime"
-    curl -sSL "https://aka.ms/dotnet/10.0/dotnet-runtime-osx-${arch}.tar.gz" | tar -C ".dotnet/${arch}-9.0-sdk+10.0-runtime" -xzf -
+    curl -sSL "https://aka.ms/dotnet/10.0/dotnet-runtime-${os}-${arch}.tar.gz" | tar -C ".dotnet/${arch}-9.0-sdk+10.0-runtime" -xzf -
     echo "===== Installing .NET SDK 9.0 with .NET Runtime 10.0 and .NET Runtime 9.0 command (${arch}) ====="
     cp -a ".dotnet/${arch}-9.0-sdk+10.0-runtime" ".dotnet/${arch}-9.0-sdk+10.0-runtime+9.0-command"
-    curl -sSL "https://aka.ms/dotnet/9.0/dotnet-runtime-osx-${arch}.tar.gz" | tar -C ".dotnet/${arch}-9.0-sdk+10.0-runtime+9.0-command" -xzf - dotnet
+    curl -sSL "https://aka.ms/dotnet/9.0/dotnet-runtime-${os}-${arch}.tar.gz" | tar -C ".dotnet/${arch}-9.0-sdk+10.0-runtime+9.0-command" -xzf - dotnet
   done
 fi
 
 echo "===== Building native library ====="
 cmake -B build -S . -D "CMAKE_OSX_ARCHITECTURES=$(
   IFS=";"
-  echo "${native_archs[*]}"
+  echo "${osx_archs[*]}"
 )"
 cmake --build build
 
